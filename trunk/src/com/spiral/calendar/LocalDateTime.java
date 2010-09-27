83bbf342d5d7f4f18170dc9bbaddccafb9426ae4
another.two <another.two@059e47e2-d9a6-34de-8b17-9655fcea41f8>
2010-09-27 22:36:49 +0000

    initial revision

---
+++ trunk/src/com/spiral/calendar/LocalDateTime.java
@@ -0,0 +1,111 @@
+package com.spiral.calendar;
+
+import java.util.Date;
+
+import com.google.gwt.i18n.client.DateTimeFormat;
+import com.google.gwt.i18n.client.TimeZone;
+
+/**
+ * A date-time without time-zone. Currently package private until API is formalized.
+ * 
+ * @author jonathan
+ * 
+ */
+class LocalDateTime
+{
+	int year;
+
+	int month;
+
+	int date;
+
+	int hours;
+
+	int minutes;
+
+	int seconds;
+
+	int milliseconds;
+
+	public Date atZone(TimeZone zone)
+	{
+		return new GWTCalendar(year, month, date, hours, minutes, seconds, zone).getTime();
+	}
+
+	@Override
+	public String toString()
+	{
+		return (year + 1900) + "-" + month + "-" + date + " " + hours + ":" + minutes + "." + seconds + "."
+				+ milliseconds;
+	}
+
+	static LocalDateTime extractFromDate(Date date, TimeZone timeZone)
+	{
+		LocalDateTime i = new LocalDateTime();
+		i.year = getYearFromDate(date, timeZone);
+		i.month = getMonthFromDate(date, timeZone);
+		i.date = getDateFromDate(date, timeZone);
+		i.hours = getHoursFromDate(date, timeZone);
+		i.minutes = getMinutesFromDate(date, timeZone);
+		i.seconds = getSecondsFromDate(date, timeZone);
+		i.milliseconds = getMillisecondsFromDate(date, timeZone);
+		return i;
+	}
+
+	private static int getYearFromDate(Date date, TimeZone timeZone)
+	{
+		
+			assert timeZone != null : "TimeZone must be set";
+			return Integer.parseInt(DateTimeFormat.getFormat("yyyy").format(date, timeZone)) - 1900;
+		
+
+	}
+
+	private static int getMonthFromDate(Date date, TimeZone timeZone)
+	{
+		
+			assert timeZone != null : "TimeZone must be set";
+			return Integer.parseInt(DateTimeFormat.getFormat("M").format(date, timeZone)) - 1;
+		
+	}
+
+	private static int getDateFromDate(Date date, TimeZone timeZone)
+	{
+		
+			assert timeZone != null : "TimeZone must be set";
+			return Integer.parseInt(DateTimeFormat.getFormat("d").format(date, timeZone));
+		
+	}
+
+	private static int getHoursFromDate(Date date, TimeZone timeZone)
+	{
+		
+			assert timeZone != null : "TimeZone must be set";
+			return Integer.parseInt(DateTimeFormat.getFormat("H").format(date, timeZone));
+		
+	}
+
+	private static int getMinutesFromDate(Date date, TimeZone timeZone)
+	{
+		
+			assert timeZone != null : "TimeZone must be set";
+			return Integer.parseInt(DateTimeFormat.getFormat("m").format(date, timeZone));
+		
+	}
+
+	private static int getSecondsFromDate(Date date, TimeZone timeZone)
+	{
+		
+			assert timeZone != null : "TimeZone must be set";
+			return Integer.parseInt(DateTimeFormat.getFormat("s").format(date, timeZone));
+		
+	}
+
+	private static int getMillisecondsFromDate(Date date, TimeZone timeZone)
+	{
+		
+			assert timeZone != null : "TimeZone must be set";
+			return Integer.parseInt(DateTimeFormat.getFormat("S").format(date, timeZone));
+		
+	}
+}
