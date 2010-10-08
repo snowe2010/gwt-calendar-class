ba929d9965c7e87ca42390c1a944e45f4603d96f
another.two <another.two@059e47e2-d9a6-34de-8b17-9655fcea41f8>
2010-10-08 16:10:35 +0000

    finished more methods in the timezone object, added locale (to support the timezone constructor that takes a locale in)

---
+++ trunk/super-source/java/util/Locale.java
@@ -0,0 +1,145 @@
+package java.util;
+
+public class Locale {
+
+	public static final Locale CANADA;
+
+	public static final Locale CANADA_FRENCH;
+
+	public static final Locale CHINA;
+
+	public static final Locale CHINESE;
+
+	public static final Locale ENGLISH;
+
+	public static final Locale FRANCE;
+
+	public static final Locale FRENCH;
+
+	public static final Locale GERMAN;
+
+	public static final Locale GERMANY;
+
+	public static final Locale ITALIAN;
+
+	public static final Locale ITALY;
+
+	public static final Locale JAPAN;
+
+	public static final Locale JAPANESE;
+
+	public static final Locale KOREA;
+
+	public static final Locale KOREAN;
+
+	public static final Locale PRC;
+
+	public static final Locale ROOT;
+
+	public static final Locale SIMPLIFIED_CHINESE;
+
+	public static final Locale TAIWAN;
+
+	public static final Locale TRADITIONAL_CHINESE;
+
+	public static final Locale UK;
+
+	public static final Locale US;
+
+
+	static{
+
+		//TODO, initialize static variable
+
+		CANADA = null;
+		CANADA_FRENCH = null;
+		CHINA = null;
+		CHINESE = null;
+		ENGLISH = null;
+		FRANCE = null;
+		FRENCH = null;
+		GERMAN = null;
+		GERMANY = null;
+		ITALIAN = null;
+		ITALY = null;
+		JAPAN = null;
+		JAPANESE = null;
+		KOREA = null;
+		KOREAN = null;
+		PRC = null;
+		ROOT = null;
+		SIMPLIFIED_CHINESE = null;
+		TAIWAN = null;
+		TRADITIONAL_CHINESE = null;
+		UK = null;
+		US = null;
+
+	}
+
+	//constructors
+
+	public Locale(String language)
+	{
+		//TODO, fix
+	}
+
+	public Locale(String language, String country)
+	{
+		//TODO, fix
+	}
+
+	public Locale(String language, String country, String variant)
+	{
+		//TODO, fix
+	}
+
+
+	//methods
+
+
+	public static Locale[] getAvailableLocales()
+	{
+		//TODO, fix
+		return null;
+	}
+
+	public String getCountry()
+	{
+		//TODO, fix
+		return null;
+	}
+
+	public static Locale getDefault()
+	{
+		//TODO, fix
+		return null;
+	}
+
+	public String getDisplayCountry()
+	{
+		//TODO, fix
+		return null;
+	}
+
+	public String getDisplayCountry(Locale inLocale)
+	{
+		//TODO, fix
+		return null;
+	}
+
+	public String getDisplayLanguage()
+	{
+		//TODO, fix
+		return null;
+	}
+
+	public String getDisplayLanguage(Locale inLocale)
+	{
+		//TODO, fix
+		return null;
+	}
+
+	//TODO, finish method creation
+
+
+}
