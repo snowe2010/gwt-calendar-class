01d6aba2bba7d1b27033e2e10a628e56192a2cf6
another.two <another.two@059e47e2-d9a6-34de-8b17-9655fcea41f8>
2010-10-10 17:34:47 +0000

    moved calendar up, more coding

--- super-source/java/util/Calendar.java
+++ super-source/java/util/Calendar.java
@@ -1,9 +1,8 @@
-package com.spiral.calendar;
+package java.util;
 
 import java.util.Date;
 
 import com.google.gwt.i18n.client.DateTimeFormat;
-import com.google.gwt.i18n.client.TimeZone;
 
 /**
  * A wrapper around the java Date object so that accessors for year, month, date, hours, minutes, and day returns values
@@ -12,7 +11,7 @@ import com.google.gwt.i18n.client.TimeZone;
  * @author jonathan
  *
  */
-public class GWTCalendar implements DateConstants
+public class Calendar implements DateConstants
 {
 
 	private TimeZone timeZone;
@@ -32,92 +31,18 @@ public class GWTCalendar implements DateConstants
 	 * nearest millisecond.
 	 */
 
-	protected GWTCalendar()
+	protected Calendar()
 	{
 
 	}
 
-	GWTCalendar(TimeZone timeZone)
+	protected Calendar(TimeZone timeZone, Locale aLocale)
 	{
 		this(new Date(), timeZone);
 	}
 
-	/**
-	 * Allocates a date and initializes it so that it represents midnight, in the given time zone, at the beginning of
-	 * the day specified by the year, month, and date arguments.
-	 *
-	 * @param year
-	 *            the year minus 1900.
-	 * @param month
-	 *            the month between 0-11.
-	 * @param date
-	 *            the day of the month between 1-31.
-	 * @param hours
-	 *            the hours between 0-23.
-	 * @param minutes
-	 *            the minutes between 0-59.
-	 * @param timeZone
-	 *            the time zone of the date
-	 */
-	public GWTCalendar(int year, int month, int date, TimeZone timeZone)
-	{
-		this(year, month, date, 0, 0, 0, timeZone);
-	}
 
-	/**
-	 * Allocates a date and initializes it so that it represents the instant at the start of the minute specified by the
-	 * year, month, date, hrs, and min arguments, in the given time zone.
-	 *
-	 * @param year
-	 *            the year minus 1900.
-	 * @param month
-	 *            the month between 0-11.
-	 * @param date
-	 *            the day of the month between 1-31.
-	 * @param hours
-	 *            the hours between 0-23.
-	 * @param minutes
-	 *            the minutes between 0-59.
-	 * @param timeZone
-	 *            the time zone of the date
-	 */
-	public GWTCalendar(int year, int month, int date, int hours, int minutes, TimeZone timeZone)
-	{
-		this(year, month, date, hours, minutes, 0, timeZone);
-	}
 
-	/**
-	 * Allocates a date and initializes it so that it represents the instant at the start of the second specified by the
-	 * year, month, date, hrs, min, and sec arguments, in the given time zone.
-	 *
-	 * @param year
-	 *            the year minus 1900.
-	 * @param month
-	 *            the month between 0-11.
-	 * @param date
-	 *            the day of the month between 1-31.
-	 * @param hours
-	 *            the hours between 0-23.
-	 * @param minutes
-	 *            the minutes between 0-59.
-	 * @param seconds
-	 *            the seconds between 0-59.
-	 * @param timeZone
-	 *            the time zone of the date
-	 */
-	public GWTCalendar(int year, int month, int date, int hours, int minutes, int seconds, TimeZone timeZone)
-	{
-		this.timeZone = timeZone;
-		localDateTime = new LocalDateTime();
-		localDateTime.year = year;
-		localDateTime.month = month;
-		localDateTime.date = date;
-		localDateTime.hours = hours;
-		localDateTime.minutes = minutes;
-		localDateTime.seconds = seconds;
-		localDateTime.milliseconds = 0;
-		needsCalculation = true;
-	}
 
 	/**
 	 * Allocates a date and initializes it to represent the same "number of milliseconds since epoch" as the given date.
@@ -125,23 +50,161 @@ public class GWTCalendar implements DateConstants
 	 * @param date
 	 *            the date which represents an instant in time since epoch
 	 */
-	public GWTCalendar(Date date, TimeZone timeZone)
+	private Calendar(Date date, TimeZone timeZone)
 	{
 		this.timeZone = timeZone;
 		setTime(date);
 	}
 
-	/**
-	 * Allocates a date and initializes it to represent the specified number of milliseconds since the standard base
-	 * time known as "the epoch", namely January 1, 1970, 00:00:00 GMT.
-	 *
-	 * @param millis
-	 *            the milliseconds since January 1, 1970, 00:00:00 GMT.
-	 */
-	public GWTCalendar(long millis, TimeZone timeZone)
+	//methods from API
+	public void add(int field, int amount)
 	{
-		this(new Date(millis), timeZone);
+		switch (field)
+		{
+			case YEAR:
+				localDateTime.year = amount;
+				needsCalculation = true;
+				break;
+
+			case MONTH:
+				localDateTime.month += amount;
+				needsCalculation = true;
+				break;
+
+			case DATE:
+				localDateTime.date += amount;
+				needsCalculation = true;
+				break;
+
+			case HOUR:
+				localDateTime.date += amount;
+				needsCalculation = true;
+				break;
+
+			case MINUTE:
+				localDateTime.minutes += amount;
+				needsCalculation = true;
+				break;
+
+			case SECOND:
+				localDateTime.seconds += amount;
+				needsCalculation = true;
+				break;
+
+			case MILLISECOND:
+				localDateTime.milliseconds += amount;
+				needsCalculation = true;
+				break;
+
+			default:
+				throw new RuntimeException("Field isn't supported yet, or bad value passed in");
+		}
 	}
+
+	public boolean after(Object when)
+	{
+		if( when instanceof Calendar)
+		{
+			return after(((Calendar)when).getTime());
+		}
+
+		throw new RuntimeException("unsupported object passed in");
+	}
+
+	public boolean after(Calendar when)
+	{
+		return after(when.getTime());
+	}
+
+	public boolean after(Date when)
+	{
+		ensureDateCalculated();
+		return calculatedDate.after(when);
+	}
+
+	public boolean before(Object when)
+	{
+		//TODO, fix
+		return false;
+	}
+
+	public void clear()
+	{
+		//TODO, fix
+	}
+
+	public void clear( int field)
+	{
+		//TODO, fix
+	}
+
+	int compareTo(Calendar anotherCalendar)
+	{
+		if( after(anotherCalendar.getTime()))
+		{
+			return 1;
+		}
+
+		if( equals(anotherCalendar))
+		{
+			return 0;
+		}
+
+		return -1;
+	}
+
+	public int get(int field)
+	{
+		switch (field)
+		{
+			case YEAR:
+				ensureDateCalculated();
+				return localDateTime.year;
+
+			case MONTH:
+				ensureDateCalculated();
+				return localDateTime.month;
+
+			case HOUR:
+				ensureDateCalculated();
+				return localDateTime.hours;
+
+			case DATE:
+				ensureDateCalculated();
+				return localDateTime.date;
+
+			case DAY_OF_WEEK:
+				return getDay();
+
+			case MINUTE:
+				ensureDateCalculated();
+				return localDateTime.minutes;
+
+			case SECOND:
+				ensureDateCalculated();
+				return localDateTime.seconds;
+
+			default:
+				throw new RuntimeException("Field isn't supported yet, or bad value passed in");
+		}
+	}
+
+	public int getActualMaximum(int field)
+	{
+		//TODO, fix
+		return 0;
+	}
+
+	public int getActualMinimum(int field)
+	{
+		//TODO, fix
+		return 0;
+	}
+
+
+	//private methods
+
+
 
 	@SuppressWarnings("deprecation")
 	private void ensureDateCalculated()
@@ -171,7 +234,7 @@ public class GWTCalendar implements DateConstants
 			localDateTime.milliseconds = (int) (date.getTime() % 1000l);
 
 			// Add time zone to the LocalDateTime to construct a real Date (thus with timezone)
-			calculatedDate = GWTCalendar.createDate(
+			calculatedDate = Calendar.createDate(
 				localDateTime.year,
 				localDateTime.month,
 				localDateTime.date,
@@ -185,9 +248,20 @@ public class GWTCalendar implements DateConstants
 		}
 	}
 
-	public static GWTCalendar getInstance(TimeZone zone)
+	public static Calendar getInstance()
 	{
-		GWTCalendar calendar = new GWTCalendar(new Date(), zone);
+		return getInstance(TimeZone.getDefault());
+	}
+
+	public static Calendar getInstance(Locale aLocale)
+	{
+		//TODO, should use locale parsing instead
+		return getInstance(TimeZone.getDefault());
+	}
+
+	public static Calendar getInstance(TimeZone zone)
+	{
+		Calendar calendar = new Calendar(new Date(), zone);
 		return calendar;
 	}
 
@@ -199,10 +273,10 @@ public class GWTCalendar implements DateConstants
 	 */
 	public void adjustToStartOfDay()
 	{
-		set(GWTCalendar.HOUR, 0);
-		set(GWTCalendar.MINUTE, 0);
-		set(GWTCalendar.SECOND, 0);
-		set(GWTCalendar.MILLISECOND, 0);
+		set(Calendar.HOUR, 0);
+		set(Calendar.MINUTE, 0);
+		set(Calendar.SECOND, 0);
+		set(Calendar.MILLISECOND, 0);
 	}
 
 	/**
@@ -213,24 +287,15 @@ public class GWTCalendar implements DateConstants
 	 */
 	public void adjustToEndOfDay()
 	{
-		set(GWTCalendar.HOUR, 23);
-		set(GWTCalendar.MINUTE, 59);
-		set(GWTCalendar.SECOND, 59);
-		set(GWTCalendar.MILLISECOND, 999);
+		set(Calendar.HOUR, 23);
+		set(Calendar.MINUTE, 59);
+		set(Calendar.SECOND, 59);
+		set(Calendar.MILLISECOND, 999);
 	}
 
-	public boolean after(GWTCalendar when)
-	{
-		return after(when.getTime());
-	}
 
-	public boolean after(Date when)
-	{
-		ensureDateCalculated();
-		return calculatedDate.after(when);
-	}
 
-	public boolean before(GWTCalendar when)
+	public boolean before(Calendar when)
 	{
 		return before(when.getTime());
 	}
@@ -285,49 +350,7 @@ public class GWTCalendar implements DateConstants
 		}
 	}
 
-	public void add(int field, int amount)
-	{
-		switch (field)
-		{
-			case YEAR:
-				localDateTime.year = amount;
-				needsCalculation = true;
-				break;
 
-			case MONTH:
-				localDateTime.month += amount;
-				needsCalculation = true;
-				break;
-
-			case DATE:
-				localDateTime.date += amount;
-				needsCalculation = true;
-				break;
-
-			case HOUR:
-				localDateTime.date += amount;
-				needsCalculation = true;
-				break;
-
-			case MINUTE:
-				localDateTime.minutes += amount;
-				needsCalculation = true;
-				break;
-
-			case SECOND:
-				localDateTime.seconds += amount;
-				needsCalculation = true;
-				break;
-
-			case MILLISECOND:
-				localDateTime.milliseconds += amount;
-				needsCalculation = true;
-				break;
-
-			default:
-				throw new RuntimeException("Field isn't supported yet, or bad value passed in");
-		}
-	}
 
 	public void setTimeZone(TimeZone timeZone)
 	{
@@ -348,41 +371,7 @@ public class GWTCalendar implements DateConstants
 		return new Date(calculatedDate.getTime());
 	}
 
-	public int get(int field)
-	{
-		switch (field)
-		{
-			case YEAR:
-				ensureDateCalculated();
-				return localDateTime.year;
 
-			case MONTH:
-				ensureDateCalculated();
-				return localDateTime.month;
-
-			case HOUR:
-				ensureDateCalculated();
-				return localDateTime.hours;
-
-			case DATE:
-				ensureDateCalculated();
-				return localDateTime.date;
-
-			case DAY_OF_WEEK:
-				return getDay();
-
-			case MINUTE:
-				ensureDateCalculated();
-				return localDateTime.minutes;
-
-			case SECOND:
-				ensureDateCalculated();
-				return localDateTime.seconds;
-
-			default:
-				throw new RuntimeException("Field isn't supported yet, or bad value passed in");
-		}
-	}
 
 	private int getDay()
 	{
@@ -391,7 +380,7 @@ public class GWTCalendar implements DateConstants
 			assert timeZone != null : "TimeZone must be set";
 
 			// TODO: See if this can be improved
-			String day = DateTimeFormat.getFormat("EEE").format(calculatedDate, timeZone);
+			String day = DateTimeFormat.getFormat("EEE").format(calculatedDate, timeZone.timeZoneContainer.getTimeZone());
 			if (day.equalsIgnoreCase("Sun"))
 			{
 				return 0;
@@ -431,7 +420,7 @@ public class GWTCalendar implements DateConstants
 
 	public void setTime(Date time)
 	{
-		this.localDateTime = LocalDateTime.extractFromDate(time, timeZone);
+		this.localDateTime = LocalDateTime.extractFromDate(time, timeZone.timeZoneContainer.getTimeZone());
 		this.calculatedDate = new Date(time.getTime());
 		needsCalculation = false;
 	}
@@ -472,9 +461,9 @@ public class GWTCalendar implements DateConstants
 
 
 		// Console.log("GWTDate createDate " + d);
-		int offsetConversion = d.getTimezoneOffset() - timeZone.getStandardOffset();
+		int offsetConversion = d.getTimezoneOffset() - timeZone.timeZoneContainer.getTimeZone().getStandardOffset();
 		d.setMinutes(d.getMinutes() - offsetConversion);
-		d.setMinutes(d.getMinutes() - timeZone.getDaylightAdjustment(d));
+		d.setMinutes(d.getMinutes() - timeZone.timeZoneContainer.getTimeZone().getDaylightAdjustment(d));
 		return d;
 	}
 
@@ -483,8 +472,8 @@ public class GWTCalendar implements DateConstants
 	public static Date revertDate(Date date, TimeZone timeZone)
 	{
 
-		int offsetConversion = date.getTimezoneOffset() - timeZone.getStandardOffset();
-		long newTime = date.getTime() - (offsetConversion + timeZone.getDaylightAdjustment(date)) * 60000;
+		int offsetConversion = date.getTimezoneOffset() - timeZone.timeZoneContainer.getTimeZone().getStandardOffset();
+		long newTime = date.getTime() - (offsetConversion + timeZone.timeZoneContainer.getTimeZone().getDaylightAdjustment(date)) * 60000;
 		Date newDate = new Date(newTime);
 		return newDate;
 	}
