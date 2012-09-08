f218bc83175c25eb582639bdb14a26f680a907e4
another.two <another.two@059e47e2-d9a6-34de-8b17-9655fcea41f8>
2012-09-08 13:13:35 +0000

    removed locale specific issue, fixes day of the week setting

--- trunk/super-source/java/util/Calendar.java
+++ trunk/super-source/java/util/Calendar.java
@@ -684,6 +684,7 @@ public class Calendar implements DateConstants
 				int day = getDay();
 				int change = amount - day;
 				add( DAY_OF_YEAR, change);
+				break;
 				
 
 			default:
@@ -828,9 +829,17 @@ public class Calendar implements DateConstants
 		return firstDayOfWeek;
 	}
 
+	private int getDay() {
+		ensureDateCalculated();
+		int y = get(YEAR);
+		int m =  1 + get(MONTH);
+		int d = 1 + get(DATE);
+		 int t[] = {0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4};
+	       y -= m < 3 ? 1 : 0;
+	       return (y + y/4 - y/100 + y/400 + t[m-1] + d) % 7;
+	}
 
-
-	private int getDay()
+	private int getDay2()
 	{
 		ensureDateCalculated();
 
